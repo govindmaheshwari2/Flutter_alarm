94f5a3d5fd8bc01ed4e10cf2a5c1393db98e7460
Govind Maheshwari <govind@Govinds-MacBook-Air.local>
2021-05-13 17:11:01 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:alarm_tutorial/homePage.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Alarm',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        appBarTheme: AppBarTheme(color: Colors.transparent,elevation: 0)
+      ),
+      home: HomePage(),
+    );
+  }
+}
